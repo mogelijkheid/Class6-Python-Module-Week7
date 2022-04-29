ddc0cd437cec524930864dbf2d45e4d33b0aeacf
Yunus <101994700+mogelijkheid@users.noreply.github.com>
2022-04-29 18:13:11 +0200

    Add files via upload

---
+++ Alphabet.py
@@ -0,0 +1,7 @@
+
+def letters_file_line(n):
+   with open("words1.txt", "w") as f:
+       alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+       letters = [alphabet[i:i + n] + "\n" for i in range(0, len(alphabet), n)]
+       f.writelines(letters)
+letters_file_line(3)
